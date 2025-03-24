13909dc1a16571a11600ea69f9b1885a14381add
Unsoftik <gg.unsoftik@gmail.com>
2025-03-24 19:43:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def login():
     return jsonify({"message": "Вход успешен!"}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
