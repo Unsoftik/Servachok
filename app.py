6f8e6aeab835c5b351cbc24ba6d36023023ee902
Unsoftik <gg.unsoftik@gmail.com>
2025-03-25 16:43:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
 
 USER_DB_FILE = 'users.json'
 
+
 def load_users():
     if os.path.exists(USER_DB_FILE):
         with open(USER_DB_FILE, 'r') as f:
             return json.load(f)
     return {}
 
+
 def save_users(users):
     with open(USER_DB_FILE, 'w') as f:
         json.dump(users, f)
+
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -24,6 +27,9 @@ def register():
     developer = request.form.get("developer", "0")  # По умолчанию 0 (не разработчик)
 
     users = load_users()
+
+    if pin_code != 1312:
+        return jsonify({"message": "Pin"}), 400
 
     if username in users:
         return jsonify({"message": "Пользователь уже существует!"}), 400
@@ -38,6 +44,7 @@ def register():
     save_users(users)
     return jsonify({"message": "Регистрация успешна!"}), 200
 
+
 @app.route("/login", methods=["POST"])
 def login():
     username = request.form.get("username")
@@ -50,12 +57,14 @@ def login():
 
     return jsonify({"message": "Вход успешен!"}), 200
 
+
 @app.route("/get_users", methods=["GET"])
 def get_users():
     users = load_users()
-    
+
     # Возвращаем пользователей с паролями и статусом разработчика
     return jsonify(users)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
