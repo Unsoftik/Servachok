9d7674e9a7164a9dfb1c98fdaf71c04d6f9484e3
Unsoftik <gg.unsoftik@gmail.com>
2025-04-01 19:30:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
+CORS(app)  # Добавляем поддержку CORS
 
 USER_DB_FILE = 'users.json'
 PROTOCOL_STATE_FILE = 'protocol.json'
